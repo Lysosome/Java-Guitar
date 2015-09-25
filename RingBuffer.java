aeb3f7575b5e70c52adc2ef18ccff056317a86df
Lysosome <ben.ma@att.net>
2015-09-25 12:01:06 -0700

    Version 1.0

--- RingBuffer.java
+++ RingBuffer.java
@@ -1,4 +1,67 @@
 public class RingBuffer
 {
-
+   //instance variables
+   private int capacity;
+   private double[] arr;
+   private int first;
+   private int last;
+   
+   public RingBuffer(int capacity)
+   {
+      this.capacity=capacity;
+      arr=new double[capacity];
+      first=0;
+      last=0;
+   }
+   
+   public int size()
+   {
+      if(last>=first)
+         return last-first;
+      return (last+capacity)-first;
+   }
+   
+   public boolean isEmpty()
+   {
+      if(last==first)
+         return true;
+      return false;
+   }
+   
+   public boolean isFull()
+   {
+      if(first-1==last || (first+capacity)-1==last)
+         return true;
+      return false;
+   }
+   
+   public void enqueue(double x)
+   {
+      if(isFull()){}
+      else if(last==capacity)
+      {
+         last=1;
+         arr[0]=x;
+      }
+      else
+      {
+         arr[last]=x;
+         last++;
+      }
+   }
+   
+   public double dequeue()
+   {
+      double ret=arr[first];
+      if(first==capacity-1)
+         first=0;
+      else
+         first++;
+      return ret;
+   }
+   
+   public double peek()
+   {
+      return arr[first];
+   }
 }
