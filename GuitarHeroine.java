02c6433fb7729e2d83ed6dcdc93cdf592dde3f58
Lysosome <ben.ma@att.net>
2015-10-01 12:10:58 -0700

    trying to access sample from visualizer thread

--- GuitarHeroine.java
+++ GuitarHeroine.java
@@ -24,10 +24,21 @@ public class GuitarHeroine {
         for (int i = 0; i < N; i++) {
             StdDraw.line(x[i], y[i], x[i+1], y[i+1]);
         }
+        
+        //Pseudocode for visualizer
+         /*
+            Make a GUI and then update the current pixel in accordance with the current value of sample.
+            -1 is at the bottom of the screen, 1 is at the top, 0 is in the middle.
+            Then advance to the next pixel and loop when you reach the end of the screen.
+         */
+     for(int i=0; i<1000; i++)    
+      System.out.println(threaded.sample());
    }
 }
 
 class Keyboard extends Thread {
+   private double sample = 0;
+   
    public void run() {
 	   final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
       final double CONCERT_A = 440.0;
@@ -53,7 +64,7 @@ class Keyboard extends Thread {
          }
    
          // compute the superposition of samples
-         double sample = 0;
+         sample = 0;
          for(int i =0; i<keys.size(); i++)
             sample+=keys.get(i).sample();
      
@@ -66,19 +77,8 @@ class Keyboard extends Thread {
             keys.get(i).tic();     
          }
          
-         
-         //Pseudocode for visualizer
-         /*
-            Make a GUI and then update the current pixel in accordance with the current value of sample.
-            -1 is at the bottom of the screen, 1 is at the top, 0 is in the middle.
-            Then advance to the next pixel and loop when you reach the end of the screen.
-         */
-         
-         //visualizer
-         /*try{
-            Thread.sleep(25);
-         } catch(InterruptedException ex){Thread.currentThread().interrupt();}
-         */      
       }
    }
+   public double sample(){return sample;}
+
 }
